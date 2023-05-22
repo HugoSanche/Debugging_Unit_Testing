7a1594449023e274e4518730ef112675495e0c18
HugoSanche <hbmclaren@gmail.com>
2023-05-21 20:01:38 -0600

    Debugging

---
+++ untitled/src/StringUtilities.java
@@ -0,0 +1,8 @@
+public class StringUtilities {
+    private StringBuilder stringBuilder=new StringBuilder();
+    private int charAdded=0;
+    public void addChar(StringBuilder stringBuilder, char c){
+        this.stringBuilder.append(c);
+        charAdded++;
+    }
+}
