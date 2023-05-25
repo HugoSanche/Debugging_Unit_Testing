53bc9a0b2784e4b3832272336598115b92ad7e57
HugoSanche <hbmclaren@gmail.com>
2023-05-24 18:59:47 -0600

    Work with Assert and handling exceptions

--- BankAccountTesting/src/BankAccountTest.java
+++ BankAccountTesting/src/BankAccountTest.java
@@ -1,10 +1,20 @@
 import static org.junit.Assert.*;
 
 public class BankAccountTest {
+    private BankAccount account;
+    private static int count;
+    @org.junit.Before
+    public void setup(){
+         account =new BankAccount("Hugo","Baltazar",1000.00,BankAccount.CHECKING);
+        System.out.println("Running a test");
+    }
+    @org.junit.BeforeClass
+    public static void beforeClass(){
+        System.out.println("This executes before any test cases");
+    }
 
     @org.junit.Test
     public void deposit() {
-        BankAccount account =new BankAccount("Hugo","Baltazar",1000.00,BankAccount.CHECKING);
         double balance=account.deposit(200.00, true);
         assertEquals(1200.00,balance,0);//Se espera que balance sea igual a 1200.00 con un margen de error de 0
                                                     // (tercer parametro)
@@ -17,23 +27,28 @@ public class BankAccountTest {
 
     @org.junit.Test
     public void getBalance_deposit() {
-        BankAccount account =new BankAccount("Hugo","Baltazar",1000.00,BankAccount.CHECKING);
         double balance=account.deposit(200.00, true);
         assertEquals(1200.00,account.getBalance(),0);//Se espera que balance sea igual a 1200.00 con un margen de error de 0
         // (tercer parametro)
     }
     @org.junit.Test
     public void getBalance_withdraw() {
-        BankAccount account =new BankAccount("Hugo","Baltazar",1000.00,BankAccount.CHECKING);
         double balance=account.withDraw(200.00, true);
         assertEquals(800.00,account.getBalance(),0);//Se espera que balance sea igual a 1200.00 con un margen de error de 0
         // (tercer parametro)
     }
     @org.junit.Test
     public void isCheking_true(){
-        BankAccount account =new BankAccount("Hugo","Baltazar",1000.00,BankAccount.SAVINGS);
         assertEquals("The account is NOT checking account",true,account.isChecking());
         assertTrue(account.isChecking());
+    }
+    @org.junit.AfterClass
+    public static void afterClass(){
+        System.out.println("This executes after any test cases. Count = "+count++);
+    }
+    @org.junit.After
+    public void tearDown(){
+        System.out.println("Count: "+count++);
     }
 
 
